1844f146cda2e69071085afe902eb0d03a426402
Glitch (lead-wide-cut) <none>
2022-07-28 09:57:49 +0000

    🌟🤑 Updated with Glitch

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening on port ${process.env.PORT || 3000}`)
 })
