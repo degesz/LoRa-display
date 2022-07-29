4a3f67cf5d81f382a0d089f0461c455b74aa1b6d
Glitch (lead-wide-cut) <none>
2022-07-29 08:42:18 +0000

    added webhook listener

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 
 
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.post("/hook", (req, res) => {
+  console.log(req.body) // Call your action on the request here
+  res.status(200).end() // Responding is important
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
